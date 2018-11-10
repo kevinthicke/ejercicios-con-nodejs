50677fde24382565d8657ee43550beccd5e7baec
Kevin Cabrera <keviincabrera@gmail.com>
2018-11-10 12:13:16 +0000

    Añadida funcionalidad básica

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const { scanf } = require('nodejs-scanf');
 const fs = require('fs');
 
-fs.readFile('./files/relativity.txt', 'utf-8', (err, data) => {
+/*fs.readFile('./files/relativity.txt', 'utf-8', (err, data) => {
     if (err) console.log('An error has ocurred');
     else console.log(data);
-});
+});*/
+
+const x = readline();
 
