67cb8fc34119ff5e329c03f5148ae16f11e0e4e1
Kevin Cabrera <keviincabrera@gmail.com>
2018-11-10 16:31:37 +0000

    last commit

--- server.js
+++ server.js
@@ -1,3 +1,31 @@
-function fdsds () {
-    dsa
+const fs = require('fs');
+const scanf = require('scanf');
+
+console.log('Enter file Name: ')
+let fileName = scanf('%s');
+
+
+const showContent = (fileName) => {
+    return new Promise ( (resolve, reject) => {   
+        const callback = (err, data) => {
+            if (err) reject(`ERROR: the file <<${fileName}>> does not exist`);
+            else {
+                let vectorOfWords = data.split(' ');
+                resolve(vectorOfWords);
+            }
+        }
+        fs.readFile('./files/' + fileName, 'utf-8', callback);
+    });
 }
+
+const writeFile = (data) => {
+    let content = '';
+    data.forEach(element => content += element + '\n');
+
+    fs.writeFile(`files/copy${fileName}`, content, err => {
+        if (err) console.log(`ERROR: I can not write a new file`);
+        else console.log(`The file has been written successfully`)
+    });
+}
+
+showContent(fileName).then(result => writeFile(result) , err => console.log(err));
